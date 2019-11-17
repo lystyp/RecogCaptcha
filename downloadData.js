47a72d36dee58fbbdc7361b0b273bc5b7cb4c641
Daniel Shi <lystyp87015@gamil.com>
2019-11-17 18:20:18 +0800

    Update download data code and train code.

--- downloadData.js
+++ downloadData.js
@@ -2,10 +2,10 @@
 var http = require('http');
 var https = require('https');
 var dev_config = require('./config/development_config')
-var fs = require('fs'), request = require('request');
+var fs = require('fs');
 
 
-function getImgResponsePromise() {
+function getImgResponsePromise(name) {
     return new Promise((resolve, reject) => {
         var options = {
             hostname: dev_config.url,
@@ -17,22 +17,19 @@ function getImgResponsePromise() {
             if (response.statusCode != '200') {
                 reject(response.statusCode);
             } else {
-                resolve(response);
+                var cookie = response.headers['set-cookie'][0].split(";")[0];
+                response.pipe(fs.createWriteStream(name).on('close', function(){
+                    console.log('Save image finish.');
+                }));
+                resolve(cookie);
             }
         });
     })
 }
 
 
-// console.log();
-// res.pipe(fs.createWriteStream("2.png").on('close', function(){
-//     console.log('done2');
-// }));
-
-
-getImgResponsePromise().then(response => {
-        console.log(response.headers)
-        cookie = response.headers['set-cookie'][0].split(";")[0];
+function getAudioResponsePromise(name, cookie) {
+    return new Promise((resolve, reject) => {
         console.log('Cookie = ' + cookie)
         var options = {
             hostname: dev_config.url,
@@ -46,11 +43,26 @@ getImgResponsePromise().then(response => {
             console.log(response.statusCode);
             console.log(response.headers);
             if (response.statusCode == '200') {
-                response.pipe(fs.createWriteStream("audio.mp3").on('close', function(){
+                response.pipe(fs.createWriteStream(name).on('close', function(){
                     console.log('Save mp3 finish.');
+                    resolve("Success");
                 }));
+            } else {
+                reject("Download audio error.");
             }
         });
-    }
-);
+    })
+}
 
+async function asyncDownloadData(image_path, audio_path, number){
+    for (var i = 0;i < number; i++) {
+        image_name = image_path + i + ".png"
+        audio_name = audio_path + i + ".mp3"
+        let cookie = await getImgResponsePromise(image_name);
+        await getAudioResponsePromise(audio_name, cookie);
+    }
+}
+
+asyncDownloadData("./test_data/image/", "./test_data/audio/", 10000).catch( err => {
+    console.log(err);
+});
--- train/train.py
+++ train/train.py
@@ -1,58 +1,80 @@
 import os
+import time
 
 import numpy as np
-import cv2
+import tensorflow as tf
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = '2'
 
 
-def get_img_list(n):
-    path = "./classify_rlt/p1/" + str(n) + "/"
-    name_list = os.listdir(path)
-    l = []
-    for img_name in name_list:
-        l.append(cv2.imread(path + img_name, cv2.IMREAD_GRAYSCALE))
-    print("Load " + str(n) + " finish.")
-    return l
+num_epochs = 20
+batch_size = 50
+learning_rate = 0.001
 
 
-def log(s):
-    print(s)
-    with open("log.txt", "a") as f:
-        f.write(s + "\n")
+class DATALoader():
+    def __init__(self):
+        self.train_data = self.get_data_list()
+        self.train_label = self.get_ground_truth_list()
+        print(self.train_data)
+        print(self.train_label)
+
+        self.train_data = self.train_data.astype(np.float32) / 255.0      # [60000, 28, 28, 1]
+        self.train_label = self.train_label.astype(np.int32)    # [60000]
+        self.num_train_data = self.train_data.shape[0]
+
+    def get_batch(self, batch_size):
+        # 从数据集中随机取出batch_size个元素并返回
+        index = np.random.randint(0, np.shape(self.train_data)[0], batch_size)
+        return self.train_data[index, :], self.train_label[index]
+
+    def get_ground_truth_list(self):
+        return np.loadtxt("./p1/p1_ground_truth.txt") - 1
+
+    def get_data_list(self):
+        return np.loadtxt("./p1/p1_raw_data.txt")
+
+
+class MLP(tf.keras.Model):
+    def __init__(self):
+        super().__init__()
+        self.dense1 = tf.keras.layers.Dense(units=100, activation=tf.nn.relu)
+        self.dense2 = tf.keras.layers.Dense(units=9)
+
+    @tf.function
+    def call(self, inputs):
+        x = self.dense1(inputs)
+        x = self.dense2(x)
+        output = tf.nn.softmax(x)
+        return output
 
 
 if __name__ == "__main__":
-    number_data = [get_img_list(1),
-                   get_img_list(2),
-                   get_img_list(3),
-                   get_img_list(4),
-                   get_img_list(5),
-                   get_img_list(6),
-                   get_img_list(7),
-                   get_img_list(8),
-                   get_img_list(9)]
+    print("Start")
 
-    raw_list = None
-    count = 0
-    number = 1
-    for img_list in number_data:
-        number_raw_list = None
-        log("Number " + str(number) + " start : " + str(count))
-        for img in img_list:
-            raw = img.reshape((1, img.shape[0] * img.shape[1]))
-            if raw_list is None:
-                raw_list = raw
-            else:
-                raw_list = np.vstack((raw_list, raw))
+    t_start = time.time()
 
-            if number_raw_list is None:
-                number_raw_list = raw
-            else:
-                number_raw_list = np.vstack((number_raw_list, raw))
-            count = count + 1
+    model = MLP()
+    data_loader = DATALoader()
+    optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+    num_batches = int(data_loader.num_train_data // batch_size * num_epochs)
+    for batch_index in range(num_batches):
+        X, y = data_loader.get_batch(batch_size)
+        with tf.GradientTape() as tape:
+            y_pred = model(X)
+            loss = tf.keras.losses.sparse_categorical_crossentropy(y_true=y, y_pred=y_pred)
+            loss = tf.reduce_mean(loss)
+            print("batch %d: loss %f" % (batch_index, loss.numpy()))
+        grads = tape.gradient(loss, model.variables)
+        optimizer.apply_gradients(grads_and_vars=zip(grads, model.variables))
 
-        np.savetxt("./classify_rlt/p1/" + str(number) + "/raw_" + str(number) + ".txt", raw_list)
-        log("Number " + str(number) + " end : " + str(count - 1))
-        log("Number " + str(number) + " len : " + str(len(img_list)))
-        log("----------------------------------------------------")
-        number = number + 1
-    np.savetxt("./classify_rlt/p1/p1_raw_data.txt", raw_list)
+    print("End")
+    print(time.time() - t_start)
+    tf.saved_model.save(model, "./p1/saved")
+
+    sparse_categorical_accuracy = tf.keras.metrics.SparseCategoricalAccuracy()
+    num_batches = int(data_loader.num_train_data // batch_size)
+    for batch_index in range(num_batches):
+        start_index, end_index = batch_index * batch_size, (batch_index + 1) * batch_size
+        y_pred = model.call(data_loader.train_data[start_index: end_index])
+        sparse_categorical_accuracy.update_state(y_true=data_loader.train_label[start_index: end_index], y_pred=y_pred)
+    print("test accuracy: %f" % sparse_categorical_accuracy.result())
